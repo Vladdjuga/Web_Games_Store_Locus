c3ba46f00e528d827c589ad239a7e625f1cdf80d
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2021-04-15 15:18:19 +0300

    add controllers and some identity

---
+++ Web_Games_Store_Locus/Services/Interfaces/IJwtTokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Games_Store_Locus.Models.Entities;
+
+namespace Web_Games_Store_Locus.Services.Interfaces
+{
+    public interface IJwtTokenService
+    {
+        public string CreateToken(User user);
+    }
+}
