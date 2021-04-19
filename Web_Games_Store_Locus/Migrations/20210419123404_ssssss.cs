5817a9295347349fbe85d3a140942223fb3f5082
Vladdjuga <55984362+Vladdjuga@users.noreply.github.com>
2021-04-19 18:00:21 +0300

    some addings

---
+++ Web_Games_Store_Locus/Migrations/20210419123404_ssssss.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Web_Games_Store_Locus.Migrations
+{
+    public partial class ssssss : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
