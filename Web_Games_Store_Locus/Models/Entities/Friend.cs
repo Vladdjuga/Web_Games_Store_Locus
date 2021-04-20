3494f7c509e20e7d55a4513ff7651735afe1f638
Kellyh7 <vanyapyhyk@gmail.com>
2021-04-20 17:03:26 +0300

    add Enities Friend

---
+++ Web_Games_Store_Locus/Models/Entities/Friend.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Games_Store_Locus.Models.Entities
+{
+    public class Friend
+    {
+        public int Id { get; set; }
+        public UserInfo Friend1 { get; set; }
+        public UserInfo Friend2 {​​​​​​​​get; set; }​​​​​​​​
+
+    }
+}
