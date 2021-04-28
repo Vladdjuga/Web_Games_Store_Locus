f4045aa4193033a7cb20ec073351674e92d980a9
Vanya <vanyapyhyk@gmail.com>
2021-04-28 17:01:02 +0300

    Add dto(2)

--- Web_Games_Store_Locus/Models/Dto/PostDto.cs
+++ Web_Games_Store_Locus/Models/Dto/PostDto.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Web_Games_Store_Locus.Models.Entities;
 
 namespace Web_Games_Store_Locus.Models.Dto
 {
     public class PostDto
     {
+        public string Id { get; set; }
+        public string Text { get; set; }
+        public string Image { get; set; }
+        public UserInfo User { get; set; }
     }
 }
